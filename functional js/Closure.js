848bfdc9062bb6de727bd50ecb39163336417a7b
tanveer_khilji <tanveerkhilji972@gmail.com>
2025-07-09 18:10:59 +0530

    closure exmple and defination explain

---
+++ functional js/Closure.js
@@ -0,0 +1,11 @@
+// closure wo hota hai jo outer function ka ander jo inner function hota hai jab wo return ya call hota hai our sath me outer variable ki value le kare return hota hai isko clusure haotah hai
+
+// what is clusure this is a outer function inside inner function return value outer function thsi is closure  for exmple
+function outer(){
+    let outerValue = "tanveer"
+    return function inner(){
+        return `my name is ${outerValue}`
+    }
+}
+let IneerFn = outer();
+console.log(IneerFn()) //my name is tanveer
